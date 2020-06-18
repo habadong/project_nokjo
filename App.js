1b90dea1e82a594a30c9f5d8bcf6f799123560b4
habadong <jiyong.ha@kakao.com>
2020-06-19 01:31:27 +0900

    key 인코딩

--- App.js
+++ App.js
@@ -11,9 +11,11 @@ import {
 import {apiGET} from './services/get';
 
 const {height, width} = Dimensions.get('window');
+const key =
+  'JYzJfcvHK1bq%2Fu2TTNgl%2B4lcrLCR7XsbDJ7b%2BGt0EP5XX5vcfBFJm4LPILroyFHj87GpIbbPVMp8EOndskzgYA%3D%3D';
 const test =
-  '/list?stDt=2015-11-18&stTm=00&edDt=2015-11-18&edTm=24&sujCode=333&fcltyMngNo=4824012333&liIndDiv=1&numOfRows=1&pageNo=1&serviceKey=fC9uHQywpBJDPJ7PqWAOUvvS2HqlTkmDRtgm3IJCkyk94BPAPOZXPZKFsG3Oh9cTHSLTs%2B5s8atJTGeKegqHJA%3D%3D';
-
+  '/list?stDt=2015-11-18&stTm=00&edDt=2015-11-18&edTm=24&sujCode=333&fcltyMngNo=4824012333&liIndDiv=1&numOfRows=1&pageNo=1&serviceKey=';
+const encoded = encodeURI(key);
 class App extends React.PureComponent {
   constructor(props) {
     super(props);
@@ -23,7 +25,7 @@ class App extends React.PureComponent {
   }
 
   _onSetData = async () => {
-    const data = await apiGET(test);
+    const data = await apiGET(test + encoded);
     console.log('@@@@@@@');
     console.log(data);
   };
@@ -37,6 +39,11 @@ class App extends React.PureComponent {
           style={styles.button}
           onPress={() => this._onSetData()}>
           <Text>button</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{...styles.button, backgroundColor: 'blue'}}
+          onPress={() => console.log(encoded)}>
+          <Text style={{color: 'white'}}>확인</Text>
         </TouchableOpacity>
       </SafeAreaView>
     );
